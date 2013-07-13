6c9a6a106ddf253799292866a6f610a8a2a880f6
Matthew Harris <harrismc@gmail.com>
2013-07-12 22:59:44 -0700

    some mongo stuff

--- game.py
+++ game.py
@@ -2,19 +2,50 @@ import requests
 
 from uuid import uuid4
 
+from pymongo import MongoClient
+
+import settings
+
+def get_mongodb():
+	client = MongoClient(host=settings.MONGODB_URL)
+	return client[settings.MONGODB_DATABASE]
 
 class Game(object):
+
+    document = None
+    _MONGODB_COLLECTION_NAME = 'snakewithus'
 
     def __init__(self, game_id=None):
         if not game_id:
             self.game_id = uuid4().urn
             self.created = True
+            # handle creation and insertion to datastore
         else:
             pass
             # hit datastore and pull game data for id
+
         self.player_urls = []
         self.local_player = None
 
+        self._mongodb = get_mongodb()
+
+    def _get_mongo_collection(self):
+        return self._mongodb[self._MONGODB_COLLECTION_NAME]
+
+    def _fetch_game(self):
+        db = self._get_mongo_collection()
+        search = db.find({id: self.game_id})
+        if len(search) != 1:
+            return None
+
+        self.document = search[0]
+
+    def save(self):
+        """saves game to mongo"""
+        db = self._get_mongo_collection()
+        db.save(self.document)
+
+
     def tick(self):
-        for url in self.player_urls:
+        for url in self.document.player_urls:
             response = requests.post()
--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ import os
 here = os.path.dirname(os.path.dirname(__file__))
 
 STATIC_FILES_DIR = os.path.join(here, './static')
+
+MONGODB_URL = 'http://'
+MONGODB_DATABASE = 'snakewithus'
