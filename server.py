18913c79c8c257559d3a11f2a5339facfbd2cdfa
Matthew Harris <harrismc@gmail.com>
2013-07-12 22:33:18 -0700

    now expects a static folder

--- server.py
+++ server.py
@@ -1,7 +1,59 @@
-from bottle import route, run, template
+from json import dumps
+from uuid import uuid4
 
-@route('/hello/<name>')
-def index(name='World'):
-        return template('<b>Hello {{name}}</b>!', name=name)
+from bottle import route, run, static_file
+from bottle import get, post, request, response
 
-    run(host='localhost', port=8080)
+from settings import *
+
+@get('/')
+def ui_index():
+    return static_file('index.html', root=STATIC_FILES_DIR)
+
+@post('/game/uistart')
+def ui_start_game():
+    """
+    expects:
+    {
+        player_urls: [
+            'url to snake client endpoint', ...
+        ],
+        local_player: true|false
+    }
+    """
+    response.content_type = 'application/json'
+    data = request.json
+
+    # init game state
+    # return a game id
+    game_state = {
+        "id": uuid4().urn
+    }
+
+    return dumps(game_state)
+
+@post('/game/tick')
+def ui_tick():
+    """
+    expects:
+    {
+        game_id: "unique-id-for-game",
+        local_player_move: "n|w|s|e"
+    }
+    """
+    response.content_type = 'application/json'
+    data = request.json
+
+
+
+    # init game state
+    # return a game id
+    game_state = {
+        "id": data.game_id
+    }
+
+    return dumps(game_state)
+
+
+run(host='localhost', port=8080)
+
---
+++ settings.py
@@ -0,0 +1,9 @@
+
+# static settings
+
+import os
+import path
+
+here = os.path.dirname(os.path.dirname(__file__))
+
+STATIC_FILES_DIR = os.path.join(here, './static')
